626ec25953c504d6f089d2f88919225bb139ee67
Douglas Quintanilha <dougquintanilha@gmail.com>
2016-02-12 23:45:29 -0200

    Tirando um syso safado

--- GerenciamentoElevadores/src/gerenciamento/Principal.java
+++ GerenciamentoElevadores/src/gerenciamento/Principal.java
@@ -70,7 +70,6 @@ public class Principal{
 	public static void main (String[] args) throws FileNotFoundException {
 		//System.out.println("O programa iniciou");
 		
-		System.out.println(args[1]);
 		
 		leEntrada(args);
 		validaEntrada();
